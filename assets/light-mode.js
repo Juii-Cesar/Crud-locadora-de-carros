729434520f6c94d86eda661d0450facb8ebf17ac
Júlio César <juliocezinha270@gmail.com>
2025-11-26 13:25:03 -0300

    adicionando modo escuro entre abas

--- assets/light-mode.js
+++ assets/light-mode.js
@@ -1,7 +1,23 @@
 const html = document.querySelector("html");
 const trilho = document.getElementById("trilho");
 
-trilho.addEventListener("click",function(){
-    html.classList.toggle("light-mode")
-    trilho.classList.toggle("light-mode")
-})
+
+const temaSalvo = localStorage.getItem("tema");
+
+if (temaSalvo === "light") {
+    html.classList.add("light-mode");
+    trilho.classList.add("light-mode");
+}
+
+
+trilho.addEventListener("click", function () {
+    html.classList.toggle("light-mode");
+    trilho.classList.toggle("light-mode");
+
+ 
+    if (html.classList.contains("light-mode")) {
+        localStorage.setItem("tema", "light");
+    } else {
+        localStorage.setItem("tema", "dark");
+    }
+});
